1d56aec3277129718570fa228ae9e5b513a276ea
Joon Park <apark702@gmail.com>
2019-04-27 02:40:34 -0400

    Update Calories.java
    updated to add additional methods and constructor

--- Calories.java
+++ Calories.java
@@ -21,16 +21,62 @@ public class Calories {
     private ArrayList<Integer> calorieHistory;
     
     public Calories(int calorieIntake){
-      calorieIntake = this.calorieIntake; 
+      this.calorieIntake = calorieIntake; 
       dailyCalorieCount = 0;
       dailyFoodCount = new ArrayList<>();
       foodCalorieValue = new ArrayList<>();
       calorieDates = new ArrayList<>();
       calorieHistory = new ArrayList<>();
     };
+    
+    public Calories(int age, int heightInInches, float weightInPounds, char gender) {
+    	
+    	//Mifflin-St Jeor Equation
+    	if (new Character(Character.toLowerCase(gender)).equals('m'))
+    		calorieIntake = (int) Math.round((weightInPounds * 0.453592 * 10) + 
+    					(2.54 * 6.25 * heightInInches) + (5 * age) + 5);
+    	else 
+    		calorieIntake = (int) Math.round((weightInPounds * 0.453592 * 10) + 
+					(2.54 * 6.25 * heightInInches) - (5 * age) - 161);  
+    	
+        dailyCalorieCount = 0;
+        dailyFoodCount = new ArrayList<>();
+        foodCalorieValue = new ArrayList<>();
+        calorieDates = new ArrayList<>();
+        calorieHistory = new ArrayList<>();    	
+    		
+    }
+    
+    public Calories (int calorieIntake, int dailyCalorieCount, 
+    		ArrayList<String> dailyFoodCount, ArrayList<Integer> foodCalorieValue) {
+    	this.calorieIntake = calorieIntake;
+    	this.dailyCalorieCount = dailyCalorieCount;
+    	this.dailyFoodCount = dailyFoodCount;
+    	this.foodCalorieValue = foodCalorieValue;
+
+        calorieDates = new ArrayList<>();
+        calorieHistory = new ArrayList<>();    
+    	
+    }
+    
+    public Calories (int calorieIntake, int dailyCalorieCount, 
+    		ArrayList<String> dailyFoodCount, ArrayList<Integer> foodCalorieValue, 
+    		ArrayList<String> calorieDates, ArrayList<Integer> calorieHistory) {
+    	this.calorieIntake = calorieIntake;
+    	this.dailyCalorieCount = dailyCalorieCount;
+    	this.dailyFoodCount = dailyFoodCount;
+    	this.foodCalorieValue = foodCalorieValue;
+        this.calorieDates = calorieDates;
+        this.calorieHistory = calorieHistory;    	
+    	
+    }
 
     public int getDailyCalorieCount(){
         return this.dailyCalorieCount;
+    }
+    
+    public int getCalorieIntake() {
+    	return this.calorieIntake;
     }
     
     public void addFood(String foodName, int calorieCount){
